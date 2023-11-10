6b17969d56ed6da743b67604dbba76b69b6cf8f4
jost3 <1427593@senati.pe>
2023-11-10 18:23:45 -0500

    app generador de qr creado

---
+++ generadorQR/generadorQR/MainPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using ZXing.Net.Mobile.Forms;
+
+namespace generadorQR
+{
+    public partial class MainPage : ContentPage
+    {
+        ZXingBarcodeImageView qr;
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void btnGeneraCodigo_Clicked(object sender, EventArgs e)
+        {
+            qr = new ZXingBarcodeImageView
+            {
+                HorizontalOptions=LayoutOptions.FillAndExpand,
+                VerticalOptions=LayoutOptions.FillAndExpand
+            };
+            qr.BarcodeFormat = ZXing.BarcodeFormat.QR_CODE;
+            qr.BarcodeOptions.Width = 500;
+            qr.BarcodeOptions.Height = 500;
+            qr.BarcodeValue = "josue cotera bravo. cargo:profesor.";
+            stkQR.Children.Add(qr);
+        }
+    }
+}
